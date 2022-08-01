b4a3ddbecea5ca610354485cf39be0ca8fb057e8
Mauricio <maucamsan@gmail.com>
2022-08-01 17:48:02 -0500

    Fix functionality or Operate
    Operate was not returning any value, since it was only calling the
    functions, but not returning their values.
    Remove the object behavior since it was not being, and just the
    functions work for the expected behavior

--- calculator.js
+++ calculator.js
@@ -1,55 +1,88 @@
 let numpad = document.querySelectorAll(".numpadValue");
 let numpadArr = Array.from(numpad);
 
+let operatorsPad = document.querySelectorAll(".operatorButton");
+let operatorArr = Array.from(operatorsPad);
+
 let display = document.querySelector("#display > input");
 
+let otherButtons = document.querySelectorAll(".extraButtons");
+let otherButsArr = Array.from(otherButtons);
 
-numpadArr.forEach(but => {but.addEventListener('click', () => 
+let firstNumber = 0;
+let secondNumber = 0;
+let currentOperator = '';
+
+
+function Add(a, b) {
+    return a + b
+}
+let add = function(a, b){
+    return a + b;
+}
+
+function Subtract(a, b) {
+    return a - b;
+}
+
+function Multiply(a, b) {
+    return a * b;
+}
+
+function Divide(a, b) {
+    return b !== 0 ? a/b : undefined;
+}
+
+function operate(operator, a, b) {
     
-    display.value += (but.textContent)
-    )
+    switch(operator){
+        case '+':
+            return Add(a, b);
+            break;
+        case '-':
+            return Subtract(a, b)
+            break;
+        case '*':
+            return Multiply(a, b)
+            break;
+        case '/':
+            return Divide(a, b)
+            break;
+        default:
+            break;
+    }
+}
 
+
+console.log(operate('+', 2, 3))
+console.log(Add(3, 3))
+
+
+
+numpadArr.forEach(but => {but.addEventListener('click', () => display.value += (but.textContent) ) });
+
+operatorArr.forEach(but => {but.addEventListener('click', function() {
+    firstNumber = parseInt(display.value);
+    currentOperator = but.textContent;
+    display.value = 0;
+    })
 });
 
-
-
-
-function Calculator() {
-    this.Add = function(a, b) {
-        return a+b
-    },
-
-    this.Subtract = function(a, b){
-        return a - b;
-    },
-
-    this.Multiply = function(a, b){
-        return a * b;
-    },
-
-    this.Divide = function(a, b){
-        return b !== 0 ? a/b : undefined;
-    },
-
-    this.Operate = function(operator, a, b){
-        switch(operator){
-            case '+':
-                this.Add(a, b)
-                break;
-            case '-':
-                this.Subtract(a, b)
-                break;
-            case '*':
-                this.Multiply(a, b)
-                break;
-            case '/':
-                this.Divide(a, b)
-                break;
-            default:
-                break;
-        }
+otherButsArr.forEach(but => {but.addEventListener('click', function(){
+    if (but.id === "equal"){
+        secondNumber = parseInt(display.value);
+        console.log(currentOperator, firstNumber, secondNumber)
+        display.value = (operate(currentOperator, firstNumber, secondNumber));
+        console.log(display.value)
     }
+    else{
+        firstNumber = 0;
+        secondNumber = 0;
+        currentOperator = '';
+        display.value = 0;
+    }
+})})
+    
 
 
-}
 
